f4390c2cfd591a04c92ae4c77197e09a827d574b
Meshwa Patel <34438435+patelmeshwa99@users.noreply.github.com>
2025-05-06 09:44:37 -0400

    Better approach for insertion sort

--- Sorting Algorithms/InsertionSort.py
+++ Sorting Algorithms/InsertionSort.py
@@ -1,13 +1,21 @@
 def insertion_sort(items):    
-    for i in range(1, len(items)):
-        index = i
-        for j in range(i)[::-1]:
-            if items[j] > items[index]:
-                temp = items[j]
-                items[j] = items[index]
-                items[index] = temp
-                index = j
+    # This has unnecessary swapping
+    # for i in range(1, len(items)):
+    #     index = i
+    #     for j in range(i)[::-1]:
+    #         if items[j] > items[index]:
+    #             temp = items[j]
+    #             items[j] = items[index]
+    #             items[index] = temp
+    #             index = j
 
-    print(items)
+    # Tried shifting here
+    for i in range(1, len(items)):
+        value = items[i]
+        j = i - 1
+        while(j >= 0 and value < items[j]):
+            items[j + 1] = items[j]
+            j -= 1
+
 items = [5, 3, 8, 4, 2]
 insertion_sort(items)
